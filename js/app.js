29ee5936b7538a87013606d0e775424e5818fd4e
Noah Heinrich <noahheinrich@gmail.com>
2016-03-23 12:51:26 -0500

    Add function to change food images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation();
+$(document).ready(function(){
+  $("#falafel").mouseenter(function(){
+    $(this).changeImg("imgs/falafel2.jpg");
+  });
+  $("#falafel").mouseleave(function(){
+    $(this).changeImg("imgs/falafel1.jpg");
+  })
+
+  jQuery.fn.extend({
+    changeImg: function (source) {
+      $(this).fadeOut("slow", function(){
+        $(this).attr("src", source).fadeIn("slow").delay("slow");
+      })
+    }
+  })
+});
